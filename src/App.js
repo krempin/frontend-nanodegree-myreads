3a49c5084f5914b3eef6edf0d3114b190e88eb8e
FK <friederike@krempin.it>
2018-08-17 16:38:02 +0200

    feat: Add the shelves with titles

--- src/App.js
+++ src/App.js
@@ -8,13 +8,6 @@ import SearchBooks from './components/SearchBooks'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
   }
 
   render() {
@@ -29,12 +22,11 @@ class BooksApp extends React.Component {
               <h1>MyReads</h1>
             </div>
             <div className="list-books-content">
-              <div>
 
-
-              <Bookshelf />
+              <Bookshelf title="Currently Reading"/>
+              <Bookshelf title="Want to Read"/>
+              <Bookshelf title="Read"/>
               
-              </div>
             </div>
             <div className="open-search">
               <Link to="/search">Add a book</Link>
--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -7,7 +7,7 @@ class Bookshelf extends React.Component{
 
     	return (
             <div className="bookshelf">
-              	<h2 className="bookshelf-title">Currently Reading</h2>
+              	<h2 className="bookshelf-title">{this.props.title}</h2>
               	<div className="bookshelf-books">
                 	<ol className="books-grid">
                 		<Book />
